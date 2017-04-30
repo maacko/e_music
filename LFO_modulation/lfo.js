d98366f138609a9b60a961eae6efa81ec0f8722f
Marco Siguencia <m08251983@gmail.com>
2017-04-29 23:33:31 -0400

    Refactoring the LFO_Modulation Project
    In the previous version of this program, I had two separate files: one was
    tasked with maintaining the audio and another was tasked with updating the style
    of the controls. Seems reasonable right? The problem was that the on/off state
    that determines the result of the tasks couldn't be shared across two different
    files unless we created a global variable for the on/off state. I wanted to
    avoid doing this, I instead maintained two isolated states in each file with the
    idea that in the abstract they we're the same. As long as the manipulations of
    the state were kept consistent, every thing would be fine. Turns out I failed
    because not even I could keep the state consistent across both files. It
    extremely easy to make an error by keeping track of the state in two different
    files. No you can understand why I refactored.
    
    The refactor isolates the setup and "exports" an interface to manipulate the
    audio and the style.  The state is kept in one context, instead of two, so there
    won't be any problems keeping track of it. One of the reasons that an interface
    is returned is because at times we need to alter a variable in a different
    scope (functions create scopes that contain references initialized with var).
    Seperating the setups into two makes writing the event handlers in the onload
    callback relatively effortless.
    
    I'll continue to refactor this as time goes on. One idea I have is to remove the
    manual styling I have in setupControls and replace by just changing the class
    name of the element. The browser will have to apply the appropriate style for
    that class when the change occurs.
    
    /e_music/LFO_Modulation
    control.js:
            - Refactored the script so that the state is maintained in one place
              instead of two places. The state wasn't even consistent across the two
              files.
    
            - The setup routines return an object containing methods necessary
              to update the audio and the styles based on the state.
    
    lfo.html:
            - Removing the controls script now that the controls are handled by
              lfo.js

--- LFO_modulation/lfo.js
+++ LFO_modulation/lfo.js
@@ -1,75 +1,111 @@
-(function () {
+(function start () {
     var on = false;
-    var audio_context = window.AudioContext || window.webkitAudioContext;
-    var context = new audio_context();
-    var osc = context.createOscillator();
-    var lfo = context.createOscillator();
-    var gain = context.createGain();
-    var osc_gain = context.createGain();
 
-    lfo.frequency.value = 10;
-    gain.gain.value = 0;
-    osc_gain.gain.value = 0;
+    function setupLFO () {
+        var audio_context = window.AudioContext || window.webkitAudioContext;
+        var context = new audio_context();
+        var osc = context.createOscillator();
+        var lfo = context.createOscillator();
+        var gain = context.createGain();
+        var osc_gain = context.createGain();
 
-    lfo.connect(gain)
-    gain.connect(osc.frequency)
-    //to control osc's volume
-    osc.connect(osc_gain);
-    osc_gain.connect(context.destination)
+        lfo.frequency.value = 10;
+        gain.gain.value = 0;
+        osc_gain.gain.value = 0;
 
-    lfo.start();
-    osc.start();
+        lfo.connect(gain)
+        gain.connect(osc.frequency)
+        //to control osc's volume
+        osc.connect(osc_gain);
+        osc_gain.connect(context.destination)
 
-    var start = function () {
-        /* You can't start an oscillator more than once. Once it's started and
-         * stopped, it can't be started again. We have to use the gain to toggle
-         * the sound
         lfo.start();
         osc.start();
-        */
-        gain.gain.value = 200;
-        //cool noisy sounds when you increase the gain from 1
-        osc_gain.gain.value = 1;
-    };
 
-    var stop = function () {
-        /*
-        lfo.stop();
-        osc.stop();
-        */
-        gain.gain.value = osc_gain.gain.value = 0;
-    };
+        return (
+        {
+            play: function () {
+            /* You can't start an oscillator more than once. Once it's started and
+            * stopped, it can't be started again. We have to use the gain to toggle
+            * the sound
+            lfo.start();
+            osc.start();
+            */
+                gain.gain.value = 200;
+            //cool noisy lo-fi sounds when you increase the gain from 1
+                osc_gain.gain.value = 1;
+            },
+            stop: function () {
+            /*
+            lfo.stop();
+            osc.stop();
+            */
+                gain.gain.value = osc_gain.gain.value = 0;
+            },
+
+            frequency: function (value){
+                lfo.frequency.value = value;
+            }
+        });
+    }
+
+    function setupControls () {
+        var changeIcon = function ( icon,
+                                    height,
+                                    width,
+                                    border_top,
+                                    border_bottom,
+                                    border_left,
+                                    background_color)
+        {
+            icon.style.height = height;
+            icon.style.width = width;
+            icon.style.borderTop = border_top;
+            icon.style.borderBottom = border_bottom;
+            icon.style.borderLeft = border_left;
+            icon.style.background = background_color;
+        };
+        var changeToPlayIcon = function (icon) {
+            changeIcon(icon, "0", "0", "20px solid transparent",
+                "20px solid transparent", "40px solid white", "none");
+        };
+        var changeToStopIcon = function (icon) {
+            changeIcon(icon, "40px", "40px", "", "", "", "white");
+        };
+
+        return (
+            {
+                changeToPlay: changeToPlayIcon,
+                changeToStop: changeToStopIcon
+            }
+        );
+    }
 
     window.addEventListener('load', function () {
         var control_button = document.getElementById('control_button');
+        var control_icon = document.getElementById('control_icon');
         var modulator = document.getElementById('modulator');
-            /*
-        stop_button.onclick = function () {
-            if (on === false) {
-                console.log('starting');
-                on = true;
-                start();
-            }
-            else {
-                console.log('stopping');
-                on = false;
-                stop();
-            }
-        };
-        */
+
+        var controls = setupControls();
+        var lfo_controls = setupLFO();
+
         control_button.addEventListener('click', function () {
 
             if (on === false) {
                 on = true;
-                start();
+                controls.changeToStop(control_icon);
+                lfo_controls.play();
             }
             else {
                 on = false;
-                stop();
+                controls.changeToPlay(control_icon);
+                lfo_controls.stop();
             }
-        })
+        });
+
         modulator.onmousemove = function (event) {
-            lfo.frequency.value = event.clientX;
-        }
+            lfo_controls.frequency(event.clientX);
+        };
     });
-})()
+
+})();
